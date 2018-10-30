5b6e196b6430d63e15841a7ff35c9a7ef87b9605
root <root@ip-172-31-29-93.ec2.internal>
2018-10-30 14:42:18 +0000

    debug

--- script.sh
+++ script.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 sudo su <<EOF
-git clone -b apache-server https://github.com/abhishekparekh1/node-express-file-form-upload-demo.git
-source sc2.sh
+source node-express-file-form-upload-demo/sc2.sh
 EOF
