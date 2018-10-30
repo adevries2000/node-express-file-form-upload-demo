3564c923ba6e14a3ffa919e419e7a3dd163d2e2b
root <root@ip-172-31-20-32.ec2.internal>
2018-10-30 14:34:16 +0000

    bash

--- user-data.sh
+++ user-data.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 apt-get update
 git clone -b apache-server https://github.com/abhishekparekh1/node-express-file-form-upload-demo.git
-
+bash node-express-file-form-upload-demo/script.sh
