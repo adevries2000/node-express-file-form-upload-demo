0f1a357fed0db8a943fd960be7afa9b0c797fa34
abhishekparekh1 <abhishekparekh1@gmail.com>
2018-11-20 10:41:29 -0500

    Update sc.sh

--- sc.sh
+++ sc.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 apt-get update
-git clone -b csv https://github.com/abhishekparekh1/node-express-file-form-upload-demo.git
+apt-get install -y default-jdk
+git clone -b java1 https://github.com/abhishekparekh1/node-express-file-form-upload-demo.git
 export NODE_HOME="/home/ubuntu/node-express-file-form-upload-demo/node-v10.13.0-linux-x64"
 export PATH="$PATH:$NODE_HOME/bin"
 cd node-express-file-form-upload-demo
