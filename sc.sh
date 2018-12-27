a306e8f49f211d17f476ef216cae1168eeafd270
root <root@ip-172-31-47-153.us-east-2.compute.internal>
2018-12-27 21:28:32 +0000

    sc.sh

--- sc.sh
+++ sc.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 apt-get update
 apt-get install -y default-jdk
-git clone -b java1 https://github.com/abhishekparekh1/node-express-file-form-upload-demo.git
+git clone https://github.com/abhishekparekh1/node-express-file-form-upload-demo.git
 export NODE_HOME="/home/ubuntu/node-express-file-form-upload-demo/node-v10.13.0-linux-x64"
 export PATH="$PATH:$NODE_HOME/bin"
 cd node-express-file-form-upload-demo
