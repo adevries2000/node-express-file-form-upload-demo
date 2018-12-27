023f6b5d828fc2491720bca972e260365cd5f96e
root <root@ip-172-31-34-75.us-east-2.compute.internal>
2018-12-27 22:10:18 +0000

    changes

--- sc.sh
+++ sc.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 apt-get update
-git clone -b csv https://github.com/abhishekparekh1/node-express-file-form-upload-demo.git
+apt-get install -y default-jdk
 export NODE_HOME="/home/ubuntu/node-express-file-form-upload-demo/node-v10.13.0-linux-x64"
 export PATH="$PATH:$NODE_HOME/bin"
-cd node-express-file-form-upload-demo
 apt-get install -y npm
 npm install
 npm install http-server -g
