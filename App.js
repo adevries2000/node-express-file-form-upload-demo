a1db4026aec9c1769e605b6174872eded841a0e0
root <root@ip-172-31-17-201.us-east-2.compute.internal>
2018-12-28 12:57:09 +0000

    update

--- App.js
+++ App.js
@@ -1,15 +1,19 @@
-/***************************************************************
-  node.js express app file form server upload w/ Multer demo
-  App created by:  Jesse Lewis
-  Multer Config used based on tutorial by Ashish Mehra via Youtube
-  @ https://www.youtube.com/watch?v=sMnqnvW81to&lc=z23htp54jwmhwni0nacdp43axbwhgu3y3fg0jwzwhatw03c010c
-******************************************************************************************************/
-
   // RUN PACKAGES
   const express = require('express');
   const multer = require('multer');
   const bodyParser = require('body-parser');
   var f = ' ';
+  var fs = require('fs');
+  var metadata = require('node-ec2-metadata');
+  var ip = '';
+metadata.getMetadataForInstance('public-ipv4')
+.then(function(ipv4) {
+    console.log("Instance ID: " + ipv4);
+    ip = ipv4;
+})
+.fail(function(error) {
+    console.log("Error: " + error);
+});
   // SETUP APP
   const app = express();
   const port = process.env.PORT || 3000;
@@ -19,28 +23,29 @@
 
 
 
-  //MULTER CONFIG: to get file photos to temp server storage
+
+
+  //MULTER CONFIG: to get files to temp server storage
   const multerConfig = {
 
     //specify diskStorage (another option is memory)
     storage: multer.diskStorage({
 
-      //specify destination
+      //specify destination: Local Directory where the files will reside
       destination: function(req, file, next){
         next(null, './public/uploads');
       },
 
-      //specify the filename to be unique
       filename: function(req, file, next){
         console.log(file);
-        //get the file mimetype ie 'image/jpeg' split and prefer the second value ie'jpeg'
+        //get the file mimetype ie 'csv' split
         const ext = file.mimetype.split('/')[1];
-        //set the file fieldname to a unique name containing the original name, current datetime and the extension.
+        //set the file fieldname to be the original name
         next(null, file.originalname);
       }
     }),
 
-    // filter out and prevent non-image files.
+    // filter out and prevent non-csv files.
     fileFilter: function(req, file, next){
           if(!file){
             next();
@@ -73,22 +78,22 @@
 
   /* ROUTES
   **********/
+
+  // index.html is the page displayes when the user types URL. This corresponds to HTTP GET request
   app.get('/', function(req, res){
     res.render('index.html');
   });
   
-
+  // This responds to HTTP POST request when a file is uploaded
   app.post('/upload', multer(multerConfig).single('excel-sheet'),function(req, res){
-      //Here is where I could add functions to then get the url of the new photo
-      //And relocate that to a cloud storage solution with a callback containing its new url
-      //then ideally loading that into your database solution.   Use case - user uploading an avatar...
+      //The business logic that processes the file
       pleaseSolve(req.body, res);
       // res.send('Complete! Navigate to  <a href="/uploads">Here</a>. To Upload more files or Try Again <a href="index.html">Click Here</a>');
   }
 
 );
 
-  // RUN SERVER
+  // RUN SERVER and listen to the port 3000
   app.listen(port,function(){
     console.log(`Server listening on port ${port}`);
   });
@@ -101,8 +106,23 @@
     // var o = +parms.param2;
     // var p = +parms.param3;
 
-    var exec = require('child_process').exec, child;    
-    child = exec('java Hello ./public/uploads/'+ f+' ' + parms.param1 +' '+ parms.param2+' '+ parms.param3,
+    var exec = require('child_process').exec, child, child3, child4, child5, child6;   // Starting the java child process to run the java code to process excel sheet 
+    
+     child3 = exec('cat ./public/index_response-1.html > ./public/index_response.html');
+   var outputLink = '<a href="http://'+ip+'/uploads/output-' + f +'">Download</a>'
+
+     child2 = exec('echo "' +outputLink+'" >> ./public/index_response.html');
+
+     // child5 = exec('echo "'+ parms.param + " " + parms.param2+ " "  + parms.param3 + " "  + parms.param4 + " "  + parms.param5 + " "  + parms.param6+ " "  +     parms.param7+ " "  + parms.param8+ " "  + parms.param9+ " "  + parms.param10+'">> ./public/index_response.html');
+
+
+     child4 = exec ('cat ./public/index_response-2.html >> ./public/index_response.html');
+
+
+     child6 = exec('echo "Done"');
+
+
+    child = exec('java -jar HerdManagement.jar ./public/uploads/'+ f +' ./public/uploads/output-' + f + " " + parms.param + " " + parms.param2+ " "  + parms.param3 + " "  + parms.param4 + " "  + parms.param5 + " "  + parms.param6+ " "  +     parms.param7+ " "  + parms.param8+ " "  + parms.param9+ " "  + parms.param10,
           function (error, stdout, stderr){
             console.log('stdout: ' + stdout);
             console.log('stderr: ' + stderr);
@@ -110,7 +130,35 @@
               console.log('exec error: ' + error);
             }
         });
+     
+   // var outputLink = '<a href="http://18.191.164.204/uploads/output-' + f +'">Download</a>'
+    
+//    var outputLink = 'sed -i \'229s/.*/ \'<a href="http://18.191.164.204/uploads/output-\''+f+'\'">Download</a>\'/' /home/ubuntu/node-express-file-form-upload-demo/public/index_response.html'
 
+
+  //  var outputLink = 'sed -i \'229s/.*/this-is-the-test/\' /home/ubuntu/node-express-file-form-upload-demo/public/index_response.html'
+
+     // child2 = exec('echo "' +outputLink+'" > outputfile.txt');
+
+     // child3 = exec('cat ./public/index_response-1.html > ./public/index_response.html');
+
+     // child2 = exec('echo "' +outputLink+'" >> ./public/index_response.html');
+
+     // // child5 = exec('echo "'+ parms.param + " " + parms.param2+ " "  + parms.param3 + " "  + parms.param4 + " "  + parms.param5 + " "  + parms.param6+ " "  +     parms.param7+ " "  + parms.param8+ " "  + parms.param9+ " "  + parms.param10+'">> ./public/index_response.html');
+
+
+     // child4 = exec ('cat ./public/index_response-2.html >> ./public/index_response.html');
+
+
+     // child6 = exec('echo "Done"');
+
+
+    //child2 = exec(outputLink);
+    // child3 = exec(' curl http://169.254.169.254/latest/meta-data/public-ipv4 > temp1.txt'') 
     res.writeHead(200, { 'Content-Type': 'text/html' });
-    res.end("The entered parameters are " + parms.param1 + " " + parms.param2+ " "  + parms.param3 );
-  }
+
+
+    // res.end("The Output File can be downloaded from <a href=http://18.191.166.171/uploads/>here</a>" + "The entered parameters are " + parms.param + " " + parms.param2+ " "  + parms.param3 + " "  + parms.param4 + " "  + parms.param5 + " "  + parms.param6+ " "  + parms.param7+ " "  + parms.param8+ " "  + parms.param9+ " "  + parms.param10 );
+    // res.end(outputLink);
+    fs.createReadStream('public/index_response.html').pipe(res);
+    }
--- sc.sh
+++ sc.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 apt-get update
 apt-get install -y default-jdk
-git clone -b java1 https://github.com/abhishekparekh1/node-express-file-form-upload-demo.git
 export NODE_HOME="/home/ubuntu/node-express-file-form-upload-demo/node-v10.13.0-linux-x64"
 export PATH="$PATH:$NODE_HOME/bin"
-cd node-express-file-form-upload-demo
 apt-get install -y npm
 npm install
+npm install node-ec2-metadata
 npm install http-server -g
 http-server -p 80 &
-node App.js &
+node App.js
