b090d1431da23a9c1b8a2bab7ecd7fdf83d359ec
root <root@ip-172-31-36-237.us-east-2.compute.internal>
2018-12-28 01:43:03 +0000

    changes

--- App.js
+++ App.js
@@ -9,7 +9,6 @@
   app.use(bodyParser.urlencoded({extended:false}));
   app.use(bodyParser.json());
   app.use('/', express.static(__dirname + '/public'));
-  app.use('/uploads', express.static(__dirname + '/public/uploads'));
 
 
 
--- sc.sh
+++ sc.sh
@@ -5,3 +5,6 @@ export NODE_HOME="/home/ubuntu/node-express-file-form-upload-demo/node-v10.13.0-
 export PATH="$PATH:$NODE_HOME/bin"
 apt-get install -y npm
 npm install
+npm install http-server -g
+http-server -p 80 &
+node App.js
